6a7bd55a300a20528d62b5530630178db7d1e425
Dave Gumba <dave_gumba@Yahoo.com>
2018-03-06 14:37:27 -0500

    header view and search bar

---
+++ kuzushi/View/HeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderView.swift
+//  kuzushi
+//
+//  Created by Dave Gumba on 2018-03-06.
+//  Copyright © 2018 Dave's Organization. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
