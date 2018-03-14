e5ec6fd98ef9b379af8c042c4565efa7a79df230
Dave Gumba <dave_gumba@Yahoo.com>
2018-03-14 14:11:30 -0400

    back

---
+++ kuzushi/View/CoinCell.swift
@@ -0,0 +1,24 @@
+//
+//  CoinCell.swift
+//  kuzushi
+//
+//  Created by Dave Gumba on 2018-03-06.
+//  Copyright © 2018 Dave's Organization. All rights reserved.
+//
+
+import UIKit
+
+class CoinCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
